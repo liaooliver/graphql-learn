6f64830af845076764d0872d43e527160ec8f59b
OliverLiao <rtw0913@gmail.com>
2019-10-22 22:33:13 +0800

    add scalar type

--- GraphQLExample/index.js
+++ GraphQLExample/index.js
@@ -1,13 +1,23 @@
 const { ApolloServer } = require('apollo-server');
+const { GraphQLScalarType } = require('graphql')
 
-var todolists = []
+var todolists = [
+    {
+        id: 1,
+        item: "String!",
+        updated: "String!",
+        created: "2019-10-10",
+        remark: "String",
+        isFinished: false
+    }
+]
 
 const typeDefs = `
     scalar DateTime
 
     type Query {
         totalItems: Int!
-        totalLists(date: String): [Lists!]!
+        totalLists(date: DateTime): [Lists!]!
     }
 
     type Mutation{
@@ -19,8 +29,8 @@ const typeDefs = `
     type Lists{
         id: ID!
         item: String!
-        updated: String!
-        created: String!
+        updated: DateTime!
+        created: DateTime!
         remark: String
         isFinished: Boolean!
     }
@@ -30,6 +40,7 @@ const resolvers = {
     Query: {
         totalItems: () => todolists.length,
         totalLists: (parent, args) => {
+            console.log("args", args)
             return args.date ? todolists.filter(list => list.created === args.date) : todolists;
         }
     },
@@ -61,14 +72,26 @@ const resolvers = {
     },
     // 使用 GraphQLScalarType 物件建立自訂純量的解析函式
     DateTime: new GraphQLScalarType({
+        // 自訂純量型態的名稱
         name: 'DateTime',
+        // 自訂型態的描述
         description: 'A valid date time value.',
-        // 把欄位查詢到的資料，回傳到 parseValue 函式上，轉換成預期的格式
-        parseValue: value => new Date(value).toLocaleString(),
-        // 把查詢變數上的資料，回傳到 serialize 函式上，轉換成預期的格式
-        serialize: value => new Date(value).toLocaleString(),
-        // 把
-        parseLiteral: ast => new Date(value).toLocaleString()
+        // 把欄位 (server端) 查詢到的資料，回傳到 serialize 函式上，轉換成預期的格式，並輸出到 client端
+        serialize: value => {
+            console.log("serialize", value)
+            return new Date(value).getTime()
+        },
+        // 把查詢變數(client端)上的資料，回傳到 parseValue 函式上，轉換成預期的格式，並輸入到 server 端
+        parseValue: value =>  new Date(value).getTime(),
+        // 把查詢引數(client端)上的資料，回傳到 serialize 函式上，轉換成預期的格式，並輸入到 server 端
+        parseLiteral: ast => {
+            console.log("ast", ast)
+            // 判斷輸入的日期是 String or Int
+            if (ast.kind === 'IntValue'){
+                ast.value = parseInt(ast.value)
+            }
+            return new Date(ast.value).toLocaleDateString()
+        }
     })
 }
 
